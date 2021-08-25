049a3317d42b948ee768cf7fa6e02563f06f8727
Vignesh Mahadevan <vigneshmr13@outlook.com>
2021-08-25 13:02:28 +0530

    initial commit

--- Day4/TradeInsertion.py
+++ Day4/TradeInsertion.py
@@ -1,9 +1,17 @@
 import json
+
 f = open('dendo_patterns.json', 'r')
 data = json.load(f)
 imperative_name = 'TradeInsertion'
-print(type(data))
+print(data)
 unique_column = 'portfolio'
+data[imperative_name]['Unique_Column'] = unique_column
+if 'Unique_Column' in data[imperative_name].keys():
+    print('Unique_Column is found')
+else:
+    print("Unique_Column is not found")
+    data[imperative_name]['Unique_Column'] = unique_column
+print(data)
 for key, value in data[imperative_name].items():
-    if key == 'Unique_Column' :
+    if key == 'Unique_Column':
         pass
