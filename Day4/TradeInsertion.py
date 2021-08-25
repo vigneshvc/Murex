b07e255dd96354c310cfecb0433d4546ecf75dfa
Vignesh Mahadevan <vigneshmr13@outlook.com>
2021-08-25 14:16:51 +0530

    almost updated commit

--- Day4/TradeInsertion.py
+++ Day4/TradeInsertion.py
@@ -1,17 +1,25 @@
 import json
 
-f = open('dendo_patterns.json', 'r')
+def getDictContent(content,unique_column):
+    for k,v in content.items():
+        if k == unique_column:
+            yield str(v)
+        if type(v) == type(dict()):
+            yield getDictContent(v,unique_column)
+def updateUniqueColumn(content,unique_column):
+    for foundItems in getDictContent(content,unique_column):
+        print(foundItems)
+    return content
+
+filename = 'dendo_patterns.json'
+f = open(filename, 'r')
 data = json.load(f)
 imperative_name = 'TradeInsertion'
 print(data)
 unique_column = 'portfolio'
 data[imperative_name]['Unique_Column'] = unique_column
-if 'Unique_Column' in data[imperative_name].keys():
-    print('Unique_Column is found')
-else:
-    print("Unique_Column is not found")
-    data[imperative_name]['Unique_Column'] = unique_column
-print(data)
-for key, value in data[imperative_name].items():
-    if key == 'Unique_Column':
-        pass
+with open(filename,'w') as outfile:
+    json.dump(data,outfile)
+f = open(data[imperative_name]['Sub_Type']+'_patterns.json','r')
+content = json.load(f)
+content = updateUniqueColumn(content,unique_column)
